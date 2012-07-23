d50071962477370f3a6fad6c701b45f8d6be41bf
Dave Spanton <d_spanton@hotmail.com>
2012-07-23 17:53:53 +0100

    more tests. added edit element function

--- src/ring_o_fun/xml.clj
+++ src/ring_o_fun/xml.clj
@@ -1,5 +1,6 @@
 (ns ring-o-fun.xml
   (:require [clojure.contrib.lazy-xml :as xml])
+  (:require [clojure.contrib.zip-filter.xml :as zf])
   (:require [clojure.zip :as zip]))
 
 (defn parse-str
@@ -14,12 +15,12 @@
   [tag coll]
   (filter (fn [e] (not= (:tag e) tag)) (:content coll)))
 
-(defn trim-start
+(defn- trim-start
   "Trims items from the front of a collection based on a 1 indexed start"
   [start-index coll]
 (drop (dec start-index) coll))
 
-(defn trim-end
+(defn- trim-end
   "Trims items from the end of a collection based on an end index"
   [end-index coll]
   (drop-last (- (count coll) end-index) coll))
@@ -29,12 +30,12 @@
   (trim-start start-index (trim-end end-index coll)))
 
 (defn trim-xml-coll
-  [coll s e]
-  (assoc coll :content (concat (get-trimmed s e (elements-with-tag :atom:entry coll)) (elements-without-tag :atom:entry coll))))
+  [tag s e coll]
+  (assoc coll :content (concat (get-trimmed s e (elements-with-tag tag coll)) (elements-without-tag tag coll))))
 
 ; set of 6, update startIndex in 0.096ms
 (defn update-content-in
-  [coll tag val]
+  [tag val coll]
   (assoc coll :content (cons {:tag tag :attrs {} :content (list val)} (filter #(not= (:tag %) tag) (:content coll)))))
 
 ; set of 6, update startIndex in 0.56ms
@@ -50,16 +51,17 @@
 
 (defn respond
   [grabbed start-index end-index]
-  (with-out-str (xml/emit (update-content-in (update-content-in (trim-xml-coll grabbed start-index end-index) :os:startIndex (str start-index)) :os:itemsPerPage (str (- end-index (dec start-index)))))))
+  (with-out-str (xml/emit (update-content-in :os:itemsPerPage (str (- end-index (dec start-index))) (update-content-in :os:startIndex (str start-index) (trim-xml-coll :atom:entry start-index end-index grabbed))))))
 
 (defn count-entries
   [coll]
   (count (filter #(= (:tag %) :atom:entry) (:content coll))))
 
 (defn ammend-entries
+  "Ammends elements within coll that have the tag 'atom:entry' by applying n and these elements to f. for example passing drop and 5 would remove the first 5 'atom:entry' elements from coll"
   [f n coll]
   (let [u-coll (assoc coll :content (concat (f n (elements-with-tag :atom:entry coll)) (elements-without-tag :atom:entry coll)))]
-  (update-content-in u-coll :os:totalResults (str (count-entries u-coll)))))
+  (update-content-in :os:totalResults (str (count-entries u-coll)) u-coll)))
 
 (defn drop-entries
   [n coll]
@@ -68,3 +70,17 @@
 (defn drop-last-entries
   [n coll]
   (ammend-entries drop-last n coll))
+
+(defn zip-route
+  [tags coll]
+  (apply (partial zf/xml-> (zip/xml-zip coll)) tags))
+
+; Test efficiency compared to the form of editing used to change the startindex property in core.
+; Fix so n is nth entries elements, not nth element that has the last route tag: effects optional tags e.g. brand title
+(defn edit-element 
+  "Edits the value of the node found at position n of the route defined by tags in the supplied collection. 'tags' is expected to be a seq of node tags that form
+   a route through an xml document, eg [:atom:entry :atom:title]."
+  ([coll tags val] (edit-element coll tags val 0))
+  ([coll tags val n] (zip/root (zip/edit (nth (zip-route tags coll) n) #(assoc %1 :content %2) (list val)))))
+
+
--- test/ring_o_fun/test/xml.clj
+++ test/ring_o_fun/test/xml.clj
@@ -3,7 +3,7 @@
   (:use [midje.sweet])
   (:import java.io.ByteArrayInputStream))
 
-(def mini-xml "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><root><length>3</length><entry i='1'>a</entry><entry i='2'>b</entry><entry i='3'>c</entry></root>")
+(def mini-xml "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><root xmlns:atom=\"http://www.w3.org/2005/Atom\"><length>3</length><atom:entry i='1'>a</atom:entry><atom:entry i='2'>b</atom:entry><atom:entry i='3'>c</atom:entry></root>")
 (def xml-stream (atom nil))
 
 (against-background [ (before :checks (reset! xml-stream (ByteArrayInputStream. (.getBytes mini-xml "UTF-8")))) ]
@@ -13,23 +13,16 @@
       (parse-str @xml-stream) => #(= "1" (get-in (second (:content %)) [:attrs :i])))
 
   (fact "should extract elements with a tag from parsed xml content"
-      (->> (parse-str @xml-stream) (elements-with-tag :entry) (count)) => 3)
+      (->> (parse-str @xml-stream) (elements-with-tag :atom:entry) (count)) => 3)
 
   (fact "should exclude elements with a tag from parsed xml content"
-      (->> (parse-str @xml-stream) (elements-without-tag :entry) first :tag) => :length))
-
-(fact "should trim the start of a collection correctly"
-      (trim-start 1 [1 2 3 4 5]) => [1 2 3 4 5]
-      (trim-start 2 [1 2 3 4 5]) => [2 3 4 5]
-      (trim-start 10 [1 2 3 4 5]) => []
-      (trim-start 0 [1 2 3 4 5]) => [1 2 3 4 5])
-
-(fact "should trim the end of a collection to an end index"
-      (trim-end 5 [1 2 3 4 5]) => [1 2 3 4 5]
-      (trim-end 3 [1 2 3 4 5]) => [1 2 3]
-      (trim-end 1 [1 2 3 4 5]) => [1]
-      (trim-end 0 [1 2 3 4 5]) => []
-      (trim-end 8 [1 2 3 4 5]) => [1 2 3 4 5])
+      (->> (parse-str @xml-stream) (elements-without-tag :atom:entry) first :tag) => :length)
+  
+  (fact "should be able to trim a parsed xml structure of items"
+      (->> (parse-str @xml-stream) (trim-xml-coll :atom:entry 2 3) (elements-with-tag :atom:entry) count) => 2)
+  
+  (fact "should be able to update the content of a single node"
+      (->> (parse-str @xml-stream) (update-content-in :length "2") (elements-with-tag :length) first :content) => '("2")))
 
 (fact "should be able to trim start and end at once"
       (get-trimmed 2 4 [1 2 3 4 5]) => [2 3 4]
@@ -38,3 +31,4 @@
       (get-trimmed 6 8 [1 2 3 4 5]) => []
       (get-trimmed 1 0 [1 2 3 4 5]) => []
       (get-trimmed 4 2 [1 2 3 4 5]) => [])
+
